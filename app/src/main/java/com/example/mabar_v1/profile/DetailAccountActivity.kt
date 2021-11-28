5fa3af6566847a1a79978702366cf9ef1573123e
Janu Dwi <janudwia@gmail.com>
2021-11-28 17:15:58 +0700

    add detail profile

---
+++ app/src/main/java/com/example/mabar_v1/profile/DetailAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mabar_v1.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mabar_v1.R
+
+class DetailAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_account)
+    }
+}
